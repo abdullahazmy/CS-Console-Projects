b0fc01640794d6ea22b79b57e9478d0aecbf29da
Abdullah Azmy <125517573+abdullahazmy@users.noreply.github.com>
2024-08-29 20:08:27 +0300

    Create Book.cs

---
+++ Abdullah/LibraryManagementSystem/Book.cs
@@ -0,0 +1,11 @@
+namespace LibraryManagementSystem;
+
+public class Book
+{
+    
+    public string Title { get; set; }
+    public string Author { get; set; }
+    
+    public string BookDescription { get; set; }
+    public int Year { get; set; }
+}
